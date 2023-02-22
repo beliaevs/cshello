f77151a4b561e98a40482918413d225ae8cd4a1b
Alexander Beliaev <abeliaev@aligntech.com>
2023-02-22 22:03:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var s = Console.ReadLine();
+Console.WriteLine(s);
+int n;
+s = Console.ReadLine();
+n = Convert.ToInt32(s);
+var s1 = Convert.ToString(n);
+Console.WriteLine(s1);
+
